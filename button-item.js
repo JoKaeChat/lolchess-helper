68caabcf8bcc12231aae570c34d4ba0eaf3fd99a
JoKaeChat <ddongbacksa@gmail.com>
2023-03-17 07:42:06 +0900

    필터 기능 완료

--- button-item.js
+++ button-item.js
@@ -1,17 +1,70 @@
-function filterSelection(item){
-   
-    let x, i;
-    x= document.getElementsByClassName("combination");
-    for(i=0;i<x.length;i++){
-        if(x[i].classList.contains(item)){
-            x[i].classList.add("show");
-            console.log("open");
-        } 
+let searches = [];
+const SEARCH_KEY = "search";
 
-        else{
-            x[i].classList.remove("show");
-            console.log("hidden");
-        }
 
-    }
+function saveSEarches(){
+    localStorage.setItem(SEARCH_KEY,JSON.stringify(searches));
 }
+
+function filterSelection(item){
+    let i,j;
+   
+    const savedSearches =localStorage.getItem(SEARCH_KEY);
+    
+
+    if( savedSearches == null){
+        console.log("비었음.")
+        searches.push(item);
+        console.log(searches);
+    }
+
+    else{
+        const parsedSearches = JSON.parse(savedSearches);
+        searches = parsedSearches;
+        const index =searches.indexOf(item);
+        if(index > -1){
+            console.log("대상 삭제");
+            searches.splice(index,1);
+        }
+        else{
+            console.log("대상 추가");
+            searches.push(item);
+        }
+    }
+    
+    const x= document.getElementsByClassName("combination");
+
+    if(searches !== null){
+      for(i=0;i<x.length;i++){
+        for(j=0; j<searches.length; j++){
+             if(x[i].classList.contains(searches[j])){
+                x[i].classList.remove("hidden");
+             } 
+             else{
+               x[i].classList.add("hidden");
+               break;
+             }
+        }  
+      }
+    } 
+
+    else{ 
+        for(i=0;i<x.length;i++){
+            x[i].classList.remove("hidden");
+        }
+    }
+
+    saveSEarches();
+}
+
+
+const itemBtns = document.getElementsByClassName("item-button");
+for(let i = 0; i<itemBtns.length; i++){
+    itemBtns[i].addEventListener("click",function(){
+        itemBtns[i].classList.toggle("active");
+    })
+}
+
+
+
+
